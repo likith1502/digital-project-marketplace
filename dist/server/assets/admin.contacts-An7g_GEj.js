15c9d9da44e8ced591af555ef099c23baad4a490
Likith <likithtalla1502@gmail.com>
2026-07-20 15:45:01 +0530

    Untrack dist folder

--- dist/server/assets/admin.contacts-An7g_GEj.js
+++
@@ -1,8 +0,0 @@
-import "react";
-import "react/jsx-runtime";
-import "lucide-react";
-import "sonner";
-//#region src/routes/admin.contacts.tsx?tsr-split=component
-var SplitComponent = () => null;
-//#endregion
-export { SplitComponent as component };
--- dist/server/assets/admin.dashboard-BkGwWQWF.js
+++
@@ -1,4 +0,0 @@
-//#region src/routes/admin.dashboard.tsx?tsr-split=component
-var SplitComponent = () => null;
-//#endregion
-export { SplitComponent as component };
